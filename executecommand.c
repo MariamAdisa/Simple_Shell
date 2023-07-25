f870931cc60b5d44263e33632ad516d53d756ac6
deeza_tb <zainabadisa425@gmail.com>
2023-07-25 15:46:27 +0100

    second

---
+++ executecommand.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * executeprogram - executes the program
+ * @argv: input string
+ */
+
+void executeprogram(char **argv)
+{
+int status;
+char *command = NULL;
+
+pid_t my_pid = fork();
+
+if (my_pid == -1)
+{
+perror("fork error");
+return;
+}
+
+if (my_pid == 0)
+{
+command = argv[0];
+if (execve(command, argv, NULL) == -1)
+{
+perror("Error:");
+exit(1);
+}
+}
+
+else
+{
+wait(&status);
+}
+}
---
+++ main.h
@@ -0,0 +1,51 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <stddef.h>
+
+#define BUFFER_SIZE 1024
+
+extern char **environ;
+
+/**
+ * struct list_t - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_t
+ */
+
+typedef struct list_t
+{
+char *dir;
+struct list_t *next;
+} list_t;
+
+void my_shell(void);
+char **parse_token(char *temp, int *ntokens);
+void executeprogram(char **argv);
+char *get_path(char *command);
+list_t *get_path_directory(char *path);
+char **_getenv(const char *var);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void assign_lineptr(char **lineptr, size_t *n, char *buffer, size_t b);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+int _strlen(const char *s);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char **_strtok(char *line, char *delim);
+int count_tokens(char *str, char *delim);
+int token_len(char *str, char *delim);
+
+#endif
